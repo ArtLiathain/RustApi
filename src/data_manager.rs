c22b4d97aee43510c90bbf3ff92ec2fc62a11a7f
ArtLiathain <artp.oliathain@gmail.com>
2024-01-28 13:57:30 +0000

    Add annualised returns test and switch asset! to asset_eq!

--- src/data_manager.rs
+++ src/data_manager.rs
@@ -1,6 +1,6 @@
 pub mod data_manager {
     use crate::stock_parsing::stock_parsing::TimeSeries;
-    use chrono::{offset, Datelike, NaiveDate};
+    use chrono::{Datelike, NaiveDate};
 
     pub fn create_stock_date_value_tuple(timeseries: TimeSeries) -> Vec<(NaiveDate, f64, f64)> {
         let mut stock_date_value_tuple: Vec<(NaiveDate, f64, f64)> = Vec::new();
@@ -51,7 +51,7 @@ pub mod data_manager {
         let number_of_years = (stock_data.last().unwrap().0 - stock_data.first().unwrap().0)
             .num_days() as f64
             / 365.00;
-        (1.00 + overall_return).powf(1.00 / number_of_years) - 1.00
+        (((1.00 + overall_return).powf(1.00 / number_of_years) - 1.00) * 10000.00).round() / 100.00
     }
 
     pub fn omega_ratio(daily_returns_data: Vec<(NaiveDate, f64)>) -> f64 {
@@ -70,7 +70,9 @@ pub mod data_manager {
         ((positive / negative.abs()) * 1000.00).round() / 1000.00
     }
 
-    pub fn rolling_returns(stock_data: Vec<(NaiveDate, f64, f64)>) -> Vec<(NaiveDate, f64)> {
+    pub fn monthly_rolling_returns(
+        stock_data: Vec<(NaiveDate, f64, f64)>,
+    ) -> Vec<(NaiveDate, f64)> {
         let mut rolling_returns: Vec<(NaiveDate, f64)> = Vec::new();
         let mut started_window = false;
         let mut offset = 0;
@@ -133,7 +135,7 @@ pub mod data_manager_testing {
                 "4. close": "158.5600",
                 "5. volume": "78460252"
                 },
-                "2022-10-31": {
+                "2022-10-29": {
                 "1. open": "140.0400",
                 "2. high": "144.7600",
                 "3. low": "135.8700",
@@ -157,8 +159,7 @@ pub mod data_manager_testing {
         let timeseries = setup().unwrap();
         let tuple = create_stock_date_value_tuple(timeseries);
         let daily_returns = super::data_manager::calculate_daily_returns(tuple);
-        print!("{:?}", daily_returns);
-        assert!(daily_returns[0].1 == -1.0);
+        assert_eq!(daily_returns[0].1 , -1.0);
     }
 
     #[test]
@@ -167,8 +168,8 @@ pub mod data_manager_testing {
         let tuple = create_stock_date_value_tuple(timeseries);
         let daily_returns = super::data_manager::calculate_daily_returns(tuple);
         let percentage = super::data_manager::calculate_percentage_of_daily_returns(daily_returns);
-        assert!(percentage.1 == 33.00);
-        assert!(percentage.0 == 67.00);
+        assert_eq!(percentage.1 , 33.00);
+        assert_eq!(percentage.0 , 67.00);
     }
 
     #[test]
@@ -177,15 +178,22 @@ pub mod data_manager_testing {
         let tuple = create_stock_date_value_tuple(timeseries);
         let daily_returns = super::data_manager::calculate_daily_returns(tuple);
         let omega_ratio = super::data_manager::omega_ratio(daily_returns);
-        assert!(omega_ratio == 12.597);
+        assert_eq!(omega_ratio, 12.597);
     }
 
     #[test]
     fn rolling_returns_test() {
         let timeseries = setup().unwrap();
         let tuple = create_stock_date_value_tuple(timeseries);
-        print!("{:?}", tuple.len());
-        let rolling_returns = super::data_manager::rolling_returns(tuple);
-        assert!(rolling_returns.len() == 2);
+        let rolling_returns = super::data_manager::monthly_rolling_returns(tuple);
+        assert_eq!(rolling_returns.len(), 2);
+    }
+
+    #[test]
+    fn annualised_returns_calculation() {
+        let timeseries = setup().unwrap();
+        let tuple = create_stock_date_value_tuple(timeseries);
+        let annualised_return = super::data_manager::calculate_annualised_returns(tuple);
+        assert_eq!(annualised_return, 8.06);
     }
 }
